28d42fc13cac55ac64a13c21f26e7a3e78bae641
Taras Ermolenko <49844905+tarasermolenko@users.noreply.github.com>
2025-06-08 12:34:40 -0700

    Mono Nuget Package dependency

---
+++ RandoopCommon/Utils/Collections.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Collections.ObjectModel;
+
+namespace Common
+{
+    public class CollectionOfCollections<T>
+    {
+        private Collection<Collection<T>> collections;
+
+        // totalSize = sum(sizes)
+        private int totalSize;
+
+        public CollectionOfCollections()
+        {
+            this.collections = new Collection<Collection<T>>();
+            this.totalSize = 0;
+        }
+
+        public void Add(Collection<T> c)
+        {
+            if (c == null) throw new ArgumentNullException();
+            collections.Add(c);
+            totalSize += c.Count;
+        }
+
+        public T Get(int i)
+        {
+            if (i < 0 || i >= totalSize) throw new ArgumentException("i");
+
+            int accum = 0;
+            for (int ci = 0; ci < collections.Count; ci++)
+            {
+                Collection<T> currColl = collections[ci];
+                if (i < accum + currColl.Count)
+                {
+                    // Desired element is in currColl.
+                    return currColl[i - accum];
+                }
+                accum += currColl.Count;
+            }
+            throw new RandoopBug();
+        }
+
+        public int Size()
+        {
+            return this.totalSize;
+        }
+
+    }
+}
